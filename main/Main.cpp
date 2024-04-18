6741f5332b120626ee207cec67e2c0af2b092b54
Galax416 <emmanuelfontaine416@gmail.com>
2024-04-18 14:31:24 +0200

    Update Main.cpp

--- main/Main.cpp
+++ main/Main.cpp
@@ -24,6 +24,7 @@ using namespace glm;
 #include <common/Camera.hpp>
 #include <common/Entity.hpp>
 #include <common/Skybox.hpp>
+#include <common/Input.hpp>
 
 
 
@@ -42,63 +43,18 @@ float zoom = 1.;
 //wire mode 
 bool wireMode = false;
 
-// Fonctions
-void processInput(GLFWwindow *window);
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+bool globalInit();
+void windowSetup();
+
 /*******************************************************************************/
 
 int main( void )
 {
-    // Initialise GLFW
-    if( !glfwInit() )
+    
+    if (!globalInit())
     {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        getchar();
         return -1;
     }
-
-    glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    // Open a window and create its OpenGL context
-    window = glfwCreateWindow( 1024, 768, "GoultardEngine", NULL, NULL);
-    if( window == NULL ){
-        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-        getchar();
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    // Initialize GLEW
-    glewExperimental = true; // Needed for core profile
-    if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Failed to initialize GLEW\n");
-        getchar();
-        glfwTerminate();
-        return -1;
-    }
-
-    // Ensure we can capture the escape key being pressed below
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-    // Hide the mouse and enable unlimited mouvement
-
-    // Set the mouse at the center of the screen
-    glfwPollEvents();
-
-    // Dark blue background
-    glClearColor(0.8f, 0.8f, 0.8f, 0.0f);
-
-    // Enable depth test
-    glEnable(GL_DEPTH_TEST);
-    // Accept fragment if it closer to the camera than the former one
-    glDepthFunc(GL_LESS);
-
-    // Cull triangles which normal is not towards the camera
-    // glEnable(GL_CULL_FACE);
 
     // Create and compile our GLSL program from the shaders
     GLuint programID = LoadShaders( "vertex_shader.glsl", "fragment_shader.glsl" );
@@ -148,7 +104,8 @@ int main( void )
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        // std::cout << round(1.0 / deltaTime) << std::endl; // Show FPS in consol
+        // Show FPS in consol
+        // std::cout << round(1.0 / deltaTime) << std::endl; 
 
         // input
         // -----
@@ -157,16 +114,16 @@ int main( void )
         // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
+        // Update
         camera_libre.update(deltaTime, window);
-
-        skybox.render(camera_libre);
-
-        scene.render(camera_libre);
-        suzane2.transform.setRotation(glm::vec3(0.0, 0.5, 0.0));
-
-
         scene.updateSelfAndChild();
+
+        // Render
+        skybox. render(camera_libre);
+        scene.  render(camera_libre);
+
+
+        suzane2.transform.setRotation(glm::vec3(0.0, 0.5, 0.0));
 
 
         // Swap buffers
@@ -186,6 +143,70 @@ int main( void )
 
     return 0;
 }
+
+
+bool globalInit()
+{
+    // Initialise GLFW
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        getchar();
+        return false;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow( 1024, 768, "GoultardEngine", NULL, NULL);
+    if( window == NULL ){
+        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+        getchar();
+        glfwTerminate();
+        return false;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return false;
+    }
+
+    windowSetup();
+    return true;
+}
+
+void windowSetup()
+{
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    // Hide the mouse and enable unlimited mouvement
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+    // Set the mouse at the center of the screen
+    glfwPollEvents();
+    //glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+
+    // Dark blue background
+    glClearColor(0.8f, 0.8f, 0.8f, 0.0f);
+
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+
+    // Cull triangles which normal is not towards the camera
+    glEnable(GL_CULL_FACE);
+}
+
 
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
